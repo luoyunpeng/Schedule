9cf09ed11f6cc8fc9147d705a03b3f048923deba
luoyunpeng <956941328@qq.com>
2017-09-01 16:23:04 +0800

    add week util

--- Schedule/src/main/java/org/schedule/util/WeekUtil.java
+++ Schedule/src/main/java/org/schedule/util/WeekUtil.java
@@ -3,8 +3,12 @@ package org.schedule.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
+import org.schedule.entity.Weekday;
 
 public class WeekUtil {
 
@@ -38,6 +42,26 @@ public class WeekUtil {
 
         return threadLocalWeek.get().format(date);
     }
+    
+    public static String getFirstDayOfLastWeek(Date date, int dayNumber) throws ParseException {
+        Calendar c = Calendar.getInstance();
+
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day - 7);
+
+        return threadLocal.get().format(c.getTime());
+    }
+
+    public static String getLastDayOfLastWeek(Date date, int dayNumber) throws ParseException {
+        Calendar c = Calendar.getInstance();
+
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day -1 );
+
+        return threadLocal.get().format(c.getTime());
+    }
 
     public static String getFirstDayOfCurrentWeek(Date date, int dayNumber) throws ParseException {
         Calendar c = Calendar.getInstance();
@@ -57,6 +81,65 @@ public class WeekUtil {
         c.set(Calendar.DATE, day + (7 - dayNumber));
 
         return threadLocal.get().format(c.getTime());
+    }
+    
+    public static String getFirstDayOfNextWeek(Date date, int dayNumber) throws ParseException {
+        Calendar c = Calendar.getInstance();
+
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day + 7);
+
+        return threadLocal.get().format(c.getTime());
+    }
+
+    public static String getLastDayOfNextWeek(Date date, int dayNumber) throws ParseException {
+        Calendar c = Calendar.getInstance();
+
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day + 13 );
+
+        return threadLocal.get().format(c.getTime());
+    }
+    
+    public  static List<Weekday> getAllWeek(Date date, int dayNumber) throws ParseException {
+        Calendar c = Calendar.getInstance();
+
+        List<Weekday> list = new ArrayList<Weekday>();
+        date = threadLocal.get().parse(getFirstDayOfCurrentWeek(date, dayNumber));
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        
+        list.add(new Weekday(1, date, getWeek(date)));
+        int tmpDay = day;
+        for (int i = 1; i <= 6; i++) {
+            c.set(Calendar.DATE, tmpDay + 1);
+            tmpDay= c.get(Calendar.DATE);
+            Date datetmp = threadLocal.get().parse(threadLocal.get().format(c.getTime()));
+            list.add(new Weekday(getWeekNumber(datetmp), datetmp, getWeek(datetmp)));
+        }
+
+        return list;
+    }
+    
+    public  static String[] getAllWeekString(Date date, int dayNumber) throws ParseException {
+        Calendar c = Calendar.getInstance();
+        String[] tmp = new String[7];
+        date = threadLocal.get().parse(getFirstDayOfCurrentWeek(date, dayNumber));
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        
+        tmp[0] = getWeek(date) + "<br/><br/>" + threadLocal.get().format(date);
+        int tmpDay = day;
+        for (int i = 1; i <= 6; i++) {
+            c.set(Calendar.DATE, tmpDay + 1);
+            tmpDay= c.get(Calendar.DATE);
+            Date datetmp = threadLocal.get().parse(threadLocal.get().format(c.getTime()));
+            tmp[i] = getWeek(datetmp) + "<br/><br/>" + threadLocal.get().format(datetmp);
+        }
+
+        return tmp;
     }
 
     public static int getWeekNumber(Date date) {
