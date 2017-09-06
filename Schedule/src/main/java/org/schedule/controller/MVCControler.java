98021f5261994fb5e5128b637f0705082e45a8d8
FengZhiWei <30582642+zhiwei-Feng@users.noreply.github.com>
2017-09-06 12:17:08 +0800

    Update MVCControler.java

--- Schedule/src/main/java/org/schedule/controller/MVCControler.java
+++ Schedule/src/main/java/org/schedule/controller/MVCControler.java
@@ -1,7 +1,5 @@
 package org.schedule.controller;
 
-import static org.hamcrest.CoreMatchers.nullValue;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
